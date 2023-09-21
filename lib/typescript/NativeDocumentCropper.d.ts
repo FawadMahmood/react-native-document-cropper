f3c2d63eb94cbf24d3a36a63b434a87b0ae7ad2f
Fawad Mahmood Siddiqui <fawad.mahmood@outlook.com>
2023-09-21 15:45:38 +0500

    chore: clean up

--- lib/typescript/NativeDocumentCropper.d.ts
+++ lib/typescript/NativeDocumentCropper.d.ts
@@ -1,9 +1,11 @@
 import type { TurboModule } from 'react-native';
-import type { IPoints } from './types';
+import type { ConvertedImageResponse, CroppedPhotoResponse, IPoints } from './types';
 export interface Spec extends TurboModule {
     multiply(a: number, b: number): Promise<number>;
     resolveImagePath(image: string): Promise<string>;
-    crop(points: IPoints, image: string): Promise<string>;
+    crop(points: IPoints, image: string): Promise<CroppedPhotoResponse>;
+    svgStringToJpg(svgString: String): Promise<ConvertedImageResponse>;
+    bmpFileToJpg(filePath: string): Promise<ConvertedImageResponse>;
 }
 declare const _default: Spec;
 export default _default;
--- lib/typescript/index.d.ts
+++ lib/typescript/index.d.ts
@@ -1,7 +1,9 @@
 import ImageCropper from './ImageCropper';
-import type { CroppedPhotoResponse, IPoints } from './types';
+import type { ConvertedImageResponse, CroppedPhotoResponse, IPoints } from './types';
 export declare function multiply(a: number, b: number): Promise<number>;
 export declare function resolveImagePath(image: string): Promise<string>;
 export declare function cropPhoto(points: IPoints, image: string): Promise<CroppedPhotoResponse>;
+export declare function svgStringToJpg(svgString: String): Promise<ConvertedImageResponse>;
+export declare function bmpFileToJpg(filePath: String): Promise<ConvertedImageResponse>;
 export default ImageCropper;
 //# sourceMappingURL=index.d.ts.map
