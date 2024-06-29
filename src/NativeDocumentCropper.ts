f8e257bf1576885a93d2b0bc7de19e2c90d88198
Fawad Mahmood Siddiqui <fawad@washmen.com>
2024-06-29 12:46:06 +0500

    feat: type fix

--- src/NativeDocumentCropper.ts
+++ src/NativeDocumentCropper.ts
@@ -1,15 +1,12 @@
 import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
-import type {
-  ConvertedImageResponse,
-  CroppedPhotoResponse,
-  IPoints,
-} from './types';
+import type { ConvertedImageResponse, CroppedPhotoResponse } from './types';
+import type { UnsafeObject } from 'react-native/Libraries/Types/CodegenTypes';
 
 export interface Spec extends TurboModule {
   multiply(a: number, b: number): Promise<number>;
   resolveImagePath(image: string): Promise<string>;
-  crop(points: IPoints, image: string): Promise<CroppedPhotoResponse>;
+  crop(points: UnsafeObject, image: string): Promise<CroppedPhotoResponse>;
   svgStringToJpg(svgString: String): Promise<ConvertedImageResponse>;
   bmpFileToJpg(filePath: string): Promise<ConvertedImageResponse>;
 }
